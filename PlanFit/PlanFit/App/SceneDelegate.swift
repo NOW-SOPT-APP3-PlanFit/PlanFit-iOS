0790324952bebc07ccbf9da1ca701253a0f96300
kingjeongkong <wjdqls9223@gmail.com>
2024-05-17 17:05:59 +0900

    [Chore/#4] Merge를 위한 SceneDelegate 초기 세팅으로 복구

--- PlanFit/PlanFit/App/SceneDelegate.swift
+++ PlanFit/PlanFit/App/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: WorkoutViewController())
+        window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
     }
 
