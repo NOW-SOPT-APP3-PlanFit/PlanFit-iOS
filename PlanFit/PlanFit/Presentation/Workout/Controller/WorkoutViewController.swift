6ab790715f6d6fda185434f2f5a58b971fd00888
kingjeongkong <wjdqls9223@gmail.com>
2024-05-17 16:16:38 +0900

    [Refactor/#4] dataSource 구성 로직 리팩토링

--- PlanFit/PlanFit/Presentation/Workout/Controller/WorkoutViewController.swift
+++ PlanFit/PlanFit/Presentation/Workout/Controller/WorkoutViewController.swift
@@ -48,17 +48,8 @@ class WorkoutViewController: UIViewController {
     private func setNavigationBar() {
         let headphoneImage = UIImage(resource: .headphone).withRenderingMode(.alwaysOriginal)
         let ellipsisImage = UIImage(resource: .ellipsis).withRenderingMode(.alwaysOriginal)
-        let headphoneItem = UIBarButtonItem(image: headphoneImage,
-                                            style: .plain,
-                                            target: self,
-                                            action: nil
-        )
-        
-        let ellipsisItem = UIBarButtonItem(image: ellipsisImage,
-                                           style: .plain,
-                                           target: self,
-                                           action: nil
-        )
+        let headphoneItem = UIBarButtonItem(image: headphoneImage, style: .plain, target: self, action: nil)
+        let ellipsisItem = UIBarButtonItem(image: ellipsisImage, style: .plain, target: self, action: nil)
         
         navigationItem.titleView = stopwatchView
         navigationItem.rightBarButtonItems = [ellipsisItem, headphoneItem]
@@ -67,106 +58,117 @@ class WorkoutViewController: UIViewController {
     }
     
     private func navigationBarColor() {
-        if #available(iOS 13.0, *) {
-            let appearance = UINavigationBarAppearance()
-            appearance.configureWithOpaqueBackground()
-            appearance.backgroundColor = .gray08BG
-            navigationController?.navigationBar.standardAppearance = appearance
-            navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
-        } else {
-            navigationController?.navigationBar.isTranslucent = false
-            navigationController?.navigationBar.barTintColor = .gray08BG
-        }
+        let appearance = UINavigationBarAppearance()
+        let navigationBar = navigationController?.navigationBar
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .gray08BG
+        navigationBar?.standardAppearance = appearance
+        navigationBar?.scrollEdgeAppearance = navigationBar?.standardAppearance
     }
     
     // MARK: - CollectionView Setting
     
     private func configureCollectionView() {
-        dataSource = UICollectionViewDiffableDataSource<Section, AnyHashable>(collectionView: rootView.collectionView, cellProvider: { collectionView, indexPath, item in
-            switch item {
-            case let workoutImage as WorkoutImageData:
-                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WorkoutImageCell.reuseIdentifier,
-                                                                    for: indexPath) as? WorkoutImageCell
-                else {
+        dataSource = UICollectionViewDiffableDataSource<Section, AnyHashable>(
+            collectionView: rootView.collectionView,
+            cellProvider: { collectionView, indexPath, item in
+                switch item {
+                case let workoutImage as WorkoutImageData:
+                    guard let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: WorkoutImageCell.reuseIdentifier,
+                        for: indexPath
+                    ) as? WorkoutImageCell else {
+                        return UICollectionViewCell()
+                    }
+                    
+                    cell.dataBind(workoutImage)
+                    return cell
+                    
+                case let setVolume as SetVolume:
+                    guard let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: WorkoutSetCell.reuseIdentifier,
+                        for: indexPath
+                    ) as? WorkoutSetCell else {
+                        return UICollectionViewCell()
+                    }
+                    
+                    cell.dataBind(setData: setVolume, setNum: indexPath.item)
+                    return cell
+                    
+                default:
                     return UICollectionViewCell()
                 }
-                
-                cell.dataBind(workoutImage)
-                return cell
-                
-            case let setVolume as SetVolume:
-                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WorkoutSetCell.reuseIdentifier,
-                                                                    for: indexPath) as? WorkoutSetCell
-                else {
-                    return UICollectionViewCell()
-                }
-                
-                cell.dataBind(setData: setVolume, setNum: indexPath.item)
-                return cell
-                
-            default:
-                return UICollectionViewCell()
             }
-        }
         )
         
         dataSource.supplementaryViewProvider = { [weak self] (collectionView, kind, indexPath) in
-            guard let self = self else { return nil }
-            guard let sectionLayoutKind = Section(rawValue: indexPath.section) else { return nil }
+            guard let self,
+                  let sectionLayoutKind = Section(rawValue: indexPath.section)
+            else {
+                return nil
+            }
             
             switch kind {
             case UICollectionView.elementKindSectionHeader:
-                
-                switch sectionLayoutKind {
-                case .WorkOutImage:
-                    guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                                           withReuseIdentifier: WorkoutImageHeader.reuseIdentifier,
-                                                                                           for: indexPath)
-                            as? WorkoutImageHeader
-                    else {
-                        return UICollectionReusableView()
-                    }
-                    return headerView
-                    
-                case .SetVolume:
-                    return UICollectionReusableView()
+                guard sectionLayoutKind == .WorkOutImage,
+                      let headerView = collectionView.dequeueReusableSupplementaryView(
+                        ofKind: kind,
+                        withReuseIdentifier: WorkoutImageHeader.reuseIdentifier,
+                        for: indexPath
+                      ) as? WorkoutImageHeader
+                else {
+                    return nil
                 }
+                return headerView
                 
             case UICollectionView.elementKindSectionFooter:
+                return configureFooterView(
+                    sectionKind: sectionLayoutKind,
+                    collectionView: collectionView,
+                    kind: kind,
+                    indexPath: indexPath
+                )
                 
-                switch sectionLayoutKind {
-                case .WorkOutImage:
-                    guard let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                                           withReuseIdentifier: WorkoutImageFooter.reuseIdentifier,
-                                                                                           for: indexPath)
-                            as? WorkoutImageFooter
-                    else {
-                        return UICollectionReusableView()
-                    }
-                    return footerView
-                    
-                case .SetVolume:
-                    guard let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                                           withReuseIdentifier: WorkoutSetFooter.reuseIdentifier,
-                                                                                           for: indexPath)
-                            as? WorkoutSetFooter
-                    else {
-                        return UICollectionReusableView()
-                    }
-                    
-                    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.addSetDidtap))
-                    footerView.addSetStackView.addGestureRecognizer(tapGestureRecognizer)
-                    
-                    return footerView
-                }
             default:
-                return UICollectionReusableView()
+                return nil
             }
         }
         
         rootView.collectionView.collectionViewLayout = setLayout()
-        
         applySectionItems()
+    }
+    
+    private func configureFooterView(
+        sectionKind: Section,
+        collectionView: UICollectionView,
+        kind: String,
+        indexPath: IndexPath
+    ) -> UICollectionReusableView {
+        switch sectionKind {
+        case .WorkOutImage:
+            guard let footerView = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: WorkoutImageFooter.reuseIdentifier,
+                for: indexPath
+            ) as? WorkoutImageFooter else {
+                return UICollectionReusableView()
+            }
+            return footerView
+            
+        case .SetVolume:
+            guard let footerView = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: WorkoutSetFooter.reuseIdentifier,
+                for: indexPath
+            ) as? WorkoutSetFooter else {
+                return UICollectionReusableView()
+            }
+            
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.addSetDidtap))
+            footerView.addSetStackView.addGestureRecognizer(tapGestureRecognizer)
+            
+            return footerView
+        }
     }
     
     private func applySectionItems() {
@@ -183,49 +185,69 @@ class WorkoutViewController: UIViewController {
             
             switch sectionLayoutKind {
             case .WorkOutImage:
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                      heightDimension: .fractionalHeight(1.0))
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
                 
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                       heightDimension: .absolute(312))
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(312)
+                )
                 let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
                 
                 let section = NSCollectionLayoutSection(group: group)
                 section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 31, bottom: 0, trailing: 32)
                 
-                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                        heightDimension: .absolute(65))
-                let footerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                        heightDimension: .absolute(20))
-                let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize,
-                                                                         elementKind: UICollectionView.elementKindSectionHeader,
-                                                                         alignment: .top)
-                let footer = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: footerSize,
-                                                                         elementKind: UICollectionView.elementKindSectionFooter,
-                                                                         alignment: .bottom)
+                let headerSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(65)
+                )
+                let footerSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(20)
+                )
+                let header = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerSize,
+                    elementKind: UICollectionView.elementKindSectionHeader,
+                    alignment: .top
+                )
+                let footer = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: footerSize,
+                    elementKind: UICollectionView.elementKindSectionFooter,
+                    alignment: .bottom
+                )
                 section.boundarySupplementaryItems = [header, footer]
                 
                 return section
                 
             case .SetVolume:
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                      heightDimension: .fractionalHeight(1.0))
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
                 
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                       heightDimension: .absolute(48))
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(48)
+                )
                 let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
                 
                 let section = NSCollectionLayoutSection(group: group)
                 section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 16, bottom: 0, trailing: 16)
                 section.interGroupSpacing = 8
                 
-                let footerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                        heightDimension: .absolute(165))
-                let footer = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: footerSize,
-                                                                         elementKind: UICollectionView.elementKindSectionFooter,
-                                                                         alignment: .bottom)
+                let footerSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(165)
+                )
+                let footer = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: footerSize,
+                    elementKind: UICollectionView.elementKindSectionFooter,
+                    alignment: .bottom
+                )
                 section.boundarySupplementaryItems = [footer]
                 
                 return section
