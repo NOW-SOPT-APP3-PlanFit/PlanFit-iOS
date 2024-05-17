7b2c041798b21e98816adee51681cb5d4b58adab
JinUng41 <ingjwjw@naver.com>
2024-05-17 11:25:40 +0900

    [Refactor/#6] 루트뷰에 운동 목표 배너 적용 및 불필요한 주석, 빈줄 삭제

--- PlanFit/PlanFit/Presentation/Home/View/HomeRootView.swift
+++ PlanFit/PlanFit/Presentation/Home/View/HomeRootView.swift
@@ -1,5 +1,5 @@
 //
-//  HomeView.swift
+//  HomeRootView.swift
 //  PlanFit
 //
 //  Created by 김진웅 on 5/14/24.
@@ -7,10 +7,13 @@
 
 import UIKit
 
-final class HomeView: UIView {
+import SnapKit
+
+final class HomeRootView: UIView {
     
     // MARK: - UIComponent
     
+    private let goalSectionView = GoalBannerView()
     
     // MARK: - Property
     
@@ -28,26 +31,25 @@ final class HomeView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - Action
-    
-    
 }
 
 // MARK: - UISetting
 
-private extension HomeView {
-    
+private extension HomeRootView {
     func setUI() {
         backgroundColor = .gray08BG
     }
     
     func setViewHierarchy() {
-        
+        addSubviews(goalSectionView)
     }
     
     func setAutoLayout() {
+        let safeArea = safeAreaLayoutGuide
         
+        goalSectionView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(safeArea).inset(16)
+        }
     }
 }
 
