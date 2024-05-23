c59f098313b57088e287d21e75d42c9d23e097bf
JinUng41 <ingjwjw@naver.com>
2024-05-23 15:35:35 +0900

    [Feat/#13] 운동 스트레칭 화면 구현

--- PlanFit/PlanFit/Presentation/WarmUp/Controller/WarmUpViewController.swift
+++ PlanFit/PlanFit/Presentation/WarmUp/Controller/WarmUpViewController.swift
@@ -5,4 +5,166 @@
 //  Created by 김진웅 on 5/14/24.
 //
 
-import Foundation
+import UIKit
+
+final class WarmUpViewController: UIViewController {
+    
+    // MARK: - UIComponent
+    
+    private let rootView = WarmUpView()
+    
+    private let stopwatchView = StopwatchView()
+    
+    // MARK: - Property
+    
+    private var timer: Timer?
+    
+    private var remainingTime = 0
+    
+    private let duration = 300
+    
+    // MARK: - LifeCycle
+    
+    override func loadView() {
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStopwatch()
+        setNavigationBar()
+        setProgressView()
+        setTarget()
+        
+        startTimer(for: duration)
+    }
+}
+
+// MARK: - Timer
+
+private extension WarmUpViewController {
+    func startTimer(for time: Int) {
+        remainingTime = time
+        updateUIForRemainingTime()
+        rootView.pauseButton.isPlaying = true
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(updateTimer),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @objc func updateTimer() {
+        if remainingTime > 0 {
+            remainingTime -= 1
+            updateUIForRemainingTime()
+        }
+    }
+    
+    func updateUIForRemainingTime() {
+        let formattedTime = formattedTime(for: remainingTime)
+        rootView.timeLabel.setText(formattedTime, font: .head01, color: .gray01)
+        rootView.progressView.setProgress(Float(duration - remainingTime) / Float(duration), animated: true)
+    }
+    
+    func formattedTime(for time: Int) -> String {
+        let minutes = time / 60
+        let seconds = time % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+}
+
+// MARK: - UI Setting
+
+private extension WarmUpViewController {
+    func setStopwatch() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(stopwatchViewDidTap))
+        stopwatchView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func setNavigationBar() {
+        let backButton = UIBarButtonItem(
+            image: .arrowLeft.withRenderingMode(.alwaysOriginal),
+            style: .plain,
+            target: self,
+            action: #selector(backButtonDidTap)
+        )
+        
+        let skipButton = UIButton().then {
+            var configuration = UIButton.Configuration.plain()
+            configuration.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            configuration.baseForegroundColor = .mainGreen
+            $0.configuration = configuration
+            $0.setTitle("웜업생략", font: .body01, titleColor: .mainGreen)
+            $0.backgroundColor = .gray06
+            $0.layer.cornerRadius = 6
+            $0.addTarget(self, action: #selector(skipButtonDidTap), for: .touchUpInside)
+        }
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.titleView = stopwatchView
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: skipButton)
+    }
+    
+    func setProgressView() {
+        rootView.progressView.setProgress(0, animated: false)
+        rootView.timeLabel.setText(formattedTime(for: duration), font: .head01, color: .gray01)
+    }
+    
+    func setTarget() {
+        let resetButton = rootView.resetButton
+        let pauseButton = rootView.pauseButton
+        let nextButton = rootView.nextButton
+        
+        resetButton.addTarget(self, action: #selector(resetButtonDidTap), for: .touchUpInside)
+        pauseButton.addTarget(self, action: #selector(pauseButtonDidTap), for: .touchUpInside)
+        nextButton.addTarget(self, action: #selector(nextButtonDidTap), for: .touchUpInside)
+    }
+    
+    // MARK: - Action
+    
+    @objc
+    func stopwatchViewDidTap() {
+        stopwatchView.isRunning.toggle()
+    }
+    
+    @objc
+    func backButtonDidTap(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc
+    func skipButtonDidTap(_ sender: UIButton) {
+        let viewController = WorkoutViewController()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    @objc 
+    func resetButtonDidTap(_ sender: UIButton) {
+        stopTimer()
+        startTimer(for: duration)
+    }
+    
+    @objc 
+    func pauseButtonDidTap(_ sender: PauseButton) {
+        sender.isPlaying.toggle()
+        sender.isPlaying ? startTimer(for: remainingTime == 0 ? duration : remainingTime) : stopTimer()
+    }
+    
+    @objc 
+    func nextButtonDidTap(_ sender: UIButton) {
+        stopTimer()
+        remainingTime = 0
+        rootView.timeLabel.setText("00:00", font: .head01, color: .gray01)
+        rootView.progressView.setProgress(1, animated: false)
+        rootView.pauseButton.isPlaying = false
+    }
+}
--- PlanFit/PlanFit/Presentation/WarmUp/View/PauseButton.swift
+++ PlanFit/PlanFit/Presentation/WarmUp/View/PauseButton.swift
@@ -5,4 +5,42 @@
 //  Created by 김진웅 on 5/23/24.
 //
 
-import Foundation
+import UIKit
+
+final class PauseButton: UIButton {
+
+    // MARK: - Property
+    
+    var isPlaying: Bool = true {
+        didSet {
+            let pauseImage = UIImage(resource: .pauseBig).withRenderingMode(.alwaysOriginal)
+            let playImage = UIImage(resource: .playBig).withRenderingMode(.alwaysOriginal)
+            setImage(isPlaying ? pauseImage : playImage, for: .normal)
+        }
+    }
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - UI Setting
+
+private extension PauseButton {
+    func setUI() {
+        backgroundColor = .gray05
+        
+        let pauseImage = UIImage(resource: .pauseBig).withRenderingMode(.alwaysOriginal)
+        setImage(pauseImage, for: .normal)
+        
+        layer.cornerRadius = 16
+    }
+}
